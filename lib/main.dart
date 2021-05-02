690606e63c2eac25d9836219d838a1a7f4826674
yuyurun <yuyu1.run@icloud.com>
2021-05-02 23:05:33 +0900

    feat: create home

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Welcome to Flutter',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,7 +22,14 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ほーむ')
+        ),
+        body: Center(
+          child: Text('はろーわーるど'),
+        ),
+      ),
     );
   }
 }
